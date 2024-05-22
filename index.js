b8572f6576e492f0ea1f292fc64bbdf382c4e862
T-Letuka <tisetsoletuka@gmail.com>
2024-05-22 15:41:51 +0200

    fixing the same error

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ app.get("/", function (req, res) {
 const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 
 // your first API endpoint...
-app.get("/api/:date", (req, res) => {
+app.get("/api/:date", function (req, res) {
   let date = new Date(req.params.date);
   if (isInvalidDate(date)) {
     date = new Date(+req.params.date);
   }
   if (isInvalidDate(date)) {
     res.json({ error: "Invalid Date" });
+    return;
   }
   res.json({
     unix: date.getTime(),
