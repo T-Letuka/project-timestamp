1e9fbf53097763a75889eab03baa5a9c1caf980b
T-Letuka <tisetsoletuka@gmail.com>
2024-05-22 15:05:12 +0200

    fixed for tests

--- index.js
+++ index.js
@@ -19,9 +19,6 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
-});
 app.get("/api/:date?", (req, res) => {
   const dateParam = req.params.date;
   let date;
